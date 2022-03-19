1149e178111fbef42a14d41bf55614a2564c0901
Bugz151995 <ryan.abugao@gmail.com>
2022-03-19 15:21:39 +0800

    reduced card width

--- src/Skills.js
+++ src/Skills.js
@@ -21,7 +21,7 @@ const logo = {
 const card_rating = {
   display: 'flex',
   margin: '0.5rem',
-  minWidth: '17rem',
+  minWidth: '16rem',
   borderRadius: '0.4rem',
   boxShadow: '0 0 10px -3px gray'
 };
